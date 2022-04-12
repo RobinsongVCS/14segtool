3f37202d68d4f691a2045cec71ac5cdd2c5caa32
Vivian <68966813+RobinsongVCS@users.noreply.github.com>
2022-04-12 17:07:41 -0500

    Initial commit

---
+++ project/LED.gd
@@ -0,0 +1,87 @@
+tool
+extends Control
+
+onready var material_led = preload("res://tex/segments.tres")
+
+var bg_color
+var off_color
+var on_color
+var bitmask
+
+signal updated(bitmask)
+
+
+func _ready():
+	material_led = material_led.duplicate()
+	$segments.material = material_led
+
+
+func _on_A_toggled(button_pressed):
+	material_led.set_shader_param("A", button_pressed)
+	update()
+
+func _on_B_toggled(button_pressed):
+	material_led.set_shader_param("B", button_pressed)
+	update()
+
+func _on_C_toggled(button_pressed):
+	material_led.set_shader_param("C", button_pressed)
+	update()
+
+func _on_D_toggled(button_pressed):
+	material_led.set_shader_param("D", button_pressed)
+	update()
+
+func _on_E_toggled(button_pressed):
+	material_led.set_shader_param("E", button_pressed)
+	update()
+
+func _on_F_toggled(button_pressed):
+	material_led.set_shader_param("F", button_pressed)
+	update()
+
+func _on_G1_toggled(button_pressed):
+	material_led.set_shader_param("G1", button_pressed)
+	update()
+
+func _on_G2_toggled(button_pressed):
+	material_led.set_shader_param("G2", button_pressed)
+	update()
+
+func _on_H_toggled(button_pressed):
+	material_led.set_shader_param("H", button_pressed)
+	update()
+
+func _on_J_toggled(button_pressed):
+	material_led.set_shader_param("J", button_pressed)
+	update()
+
+func _on_K_toggled(button_pressed):
+	material_led.set_shader_param("K", button_pressed)
+	update()
+
+func _on_L_toggled(button_pressed):
+	material_led.set_shader_param("L", button_pressed)
+	update()
+
+func _on_M_toggled(button_pressed):
+	material_led.set_shader_param("M", button_pressed)
+	update()
+
+func _on_N_toggled(button_pressed):
+	material_led.set_shader_param("N", button_pressed)
+	update()
+
+func _on_DP_toggled(button_pressed):
+	material_led.set_shader_param("DP", button_pressed)
+	update()
+
+func update_colors():
+	material.set_shader_param("bg",bg_color)
+	material.set_shader_param("off",off_color)
+	material_led.set_shader_param("on",on_color)
+
+func update():
+	emit_signal("updated",bitmask)
+	
+	
---
+++ project/main.gd
@@ -0,0 +1,125 @@
+tool
+extends Control
+
+var bg_color
+var off_color
+var on_color
+
+onready var alert_container = $alert_container
+onready var bg_dialog = $alert_container/bg_dialog
+onready var off_dialog = $alert_container/off_dialog
+onready var on_dialog = $alert_container/on_dialog
+onready var bg_panel = $Panel/CenterContainer/Panel/bg_panel
+
+onready var bg_button = $Panel/CenterContainer/VBoxContainer/HBoxContainer/select_bg
+var bg_button_style = StyleBoxFlat.new()
+var bg_panel_style
+
+onready var off_button = $Panel/CenterContainer/VBoxContainer/HBoxContainer/select_off
+var off_button_style
+
+onready var on_button = $Panel/CenterContainer/VBoxContainer/HBoxContainer/select_on
+var on_button_style
+
+onready var led1 = $Panel/CenterContainer/VBoxContainer/LED_container/LED1
+onready var led2 = $Panel/CenterContainer/VBoxContainer/LED_container/LED2
+onready var led3 = $Panel/CenterContainer/VBoxContainer/LED_container/LED3
+onready var led4 = $Panel/CenterContainer/VBoxContainer/LED_container/LED4
+onready var leds = [led1,led2,led3,led4]
+
+onready var camera = $Camera2D
+
+func _ready():
+	OS.center_window()
+# warning-ignore:return_value_discarded
+	get_tree().root.connect("size_changed", self, "_on_viewport_size_changed")
+	
+	bg_button_style.corner_radius_bottom_left = 2
+	bg_button_style.corner_radius_bottom_right = 2
+	bg_button_style.corner_radius_top_left = 2
+	bg_button_style.corner_radius_top_right = 2
+	bg_panel_style = bg_button_style.duplicate()
+	bg_button_style.border_color = Color.gray
+	bg_button_style.border_width_bottom = 2
+	bg_button_style.border_width_top = 2
+	bg_button_style.border_width_left = 2
+	bg_button_style.border_width_right = 2
+	off_button_style = bg_button_style.duplicate()
+	on_button_style = bg_button_style.duplicate()
+
+	bg_color = Color(.01,.01,.01)
+	off_color = Color(.2,.0,.0)
+	on_color = Color(.9,.0,.0)
+	
+	bg_button_style.bg_color = bg_color
+	bg_panel_style.bg_color = bg_color
+	off_button_style.bg_color = off_color
+	on_button_style.bg_color = on_color
+	update_colors()
+	
+	bg_panel.add_stylebox_override("panel", bg_panel_style)
+	
+	bg_button.add_stylebox_override("normal", bg_button_style)
+	off_button.add_stylebox_override("normal", off_button_style)
+	on_button.add_stylebox_override("normal", on_button_style)
+	
+	bg_button.add_stylebox_override("hover", bg_button_style)
+	off_button.add_stylebox_override("hover", off_button_style)
+	on_button.add_stylebox_override("hover", on_button_style)
+
+
+func _on_select_bg_pressed():
+
+	alert_container.visible = true
+	bg_dialog.visible = true
+	
+func _on_bg_color_changed(color):
+	bg_color = color
+	bg_button_style.bg_color = color
+	bg_panel_style.bg_color = bg_color
+	bg_button.add_stylebox_override("normal", bg_button_style)
+	bg_button.add_stylebox_override("hover", bg_button_style)
+	bg_panel.add_stylebox_override("panel", bg_panel_style)
+	update_colors()
+	
+func _on_bg_dialog_confirmed():
+	alert_container.visible = false
+
+func _on_select_off_pressed():
+	alert_container.visible = true
+	off_dialog.visible = true
+
+func _on_off_color_changed(color):
+	off_color = color
+	off_button_style.bg_color = color
+	off_button.add_stylebox_override("normal", off_button_style)
+	off_button.add_stylebox_override("hover", off_button_style)
+	update_colors()
+	
+func _on_off_dialog_confirmed():
+	alert_container.visible = false
+
+func _on_select_on_pressed():
+	alert_container.visible = true
+	on_dialog.visible = true
+
+func _on_on_color_changed(color):
+	on_color = color
+	on_button_style.bg_color = color
+	on_button.add_stylebox_override("normal", on_button_style)
+	on_button.add_stylebox_override("hover", on_button_style)
+	update_colors()
+
+func _on_on_dialog_confirmed():
+	alert_container.visible = false
+	
+func update_colors():
+	for led in leds:
+		led.bg_color = bg_color
+		led.off_color = off_color
+		led.on_color = on_color
+		led.update_colors()
+		
+func _on_viewport_size_changed():
+	rect_size = OS.window_size
+	camera.offset = rect_size/2
